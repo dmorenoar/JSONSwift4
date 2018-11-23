adbc4faf5e6bae896d67b0744bc64f1a29e4d3dc
dmorenoar <dmorenoar@dmorenoars-mac.local>
2018-11-23 12:34:20 -0800

    Initial Commit

---
+++ JSONSwift4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONSwift4
+//
+//  Created by dmorenoar on 23/11/18.
+//  Copyright © 2018 dmorenoar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
